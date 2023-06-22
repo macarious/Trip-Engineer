7c54b49740bdf70100ae02bec5c260f15ef96309
macarious <63441014+macarious@users.noreply.github.com>
2023-06-22 02:41:34 -0700

    Add cards to show schedule below form

--- client/src/components/TravelForm.jsx
+++ client/src/components/TravelForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Navigate } from "react-router-dom";
-import { Form, Button, ButtonGroup } from 'react-bootstrap';
+import { Button, ButtonGroup, Card, Col, Form, ListGroup, Row, Spinner } from 'react-bootstrap';
 import { useAuthToken } from "../AuthTokenContext";
 import useTravelPlans from "../hooks/useTravelPlans";
 
@@ -32,7 +32,6 @@ export default function TravelForm() {
 
     // This function is called when plan generation is successful
     function successfulOutcome() {
-        alert("Plan generated successfully. Plans can be found in the \"Saved Plans\" page.");
         setLoading(false);
     };
 
@@ -158,112 +157,160 @@ export default function TravelForm() {
     };
 
     return (
-        <Form
-            noValidate
-            validated={validated}
-            className="d-grid"
-            onSubmit={handleFormSubmit}
-        >
-            <Form.Group controlId="location">
-                <Form.Label
-                    aria-label="Destination"
-                    className="fw-bold mt-3"
-                    >Destination
-                </Form.Label>
-                <Form.Control
-                    name="location"
-                    type="text"
-                    placeholder="ex. Vancouver, BC"
-                    aria-label="Destination"
-                    aria-describedby="Enter destination; ex. Vancouver, BC or Sydney, Australia"
-                    pattern=".{5,50}"
-                    onChange={handleFieldChange}
-                    required
-                />
-                <Form.Control.Feedback type="invalid">Please input a destination (5 to 50 characters)</Form.Control.Feedback>
-            </Form.Group>
+        <>
+            <Form
+                noValidate
+                validated={validated}
+                className="d-grid"
+                onSubmit={handleFormSubmit}
+            >
+                <Form.Group controlId="location">
+                    <Form.Label
+                        aria-label="Destination"
+                        className="fw-bold mt-3"
+                        >Destination
+                    </Form.Label>
+                    <Form.Control
+                        name="location"
+                        type="text"
+                        placeholder="ex. Vancouver, BC"
+                        aria-label="Destination"
+                        aria-describedby="Enter destination; ex. Vancouver, BC or Sydney, Australia"
+                        pattern=".{5,50}"
+                        onChange={handleFieldChange}
+                        required
+                    />
+                    <Form.Control.Feedback type="invalid">Please input a destination (5 to 50 characters)</Form.Control.Feedback>
+                </Form.Group>
 
-            <Form.Group controlId="durationDays">
-                <Form.Label className="fw-bold mt-3">Duration (in days)</Form.Label>
-                <ButtonGroup className="d-flex">
-                    {[...Array(5)].map((_, index) => (
+                <Form.Group controlId="durationDays">
+                    <Form.Label className="fw-bold mt-3">Duration (in days)</Form.Label>
+                    <ButtonGroup className="d-flex">
+                        {[...Array(5)].map((_, index) => (
+                        <Button
+                            key={index + 1}
+                            variant={durationDays === (index + 1) ? "success" : "outline-secondary"}
+                            aria-label={`Duration ${index + 1} Day(s)`}
+                            aria-describedby={`Duration ${index + 1} Day(s)`}
+                            onClick={() => handleDurationButtonClick(index + 1)}
+                            className="d-flex d-flex justify-content-center fw-bold"
+                        >
+                            {index + 1}
+                        </Button>
+                        ))}
+                    </ButtonGroup>
+                </Form.Group>
+
+                <Form.Group controlId="arrivalTime">
+                    <Form.Label className="fw-bold mt-3">Arrival Time</Form.Label>
+                    <Form.Control
+                        type="time"
+                        name="arrivalTime"
+                        value={formData.arrivalTime}
+                        aria-label="Arrival Time"
+                        aria-describedby="Enter arrival time of first day"
+                        onChange={handleFieldChange}
+                        required
+                    />
+                    <Form.Control.Feedback type="invalid">Please select an arrival time</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group controlId="departureTime">
+                    <Form.Label className="fw-bold mt-3">Departure Time</Form.Label>
+                    <Form.Control
+                        type="time"
+                        name="departureTime"
+                        value={formData.departureTime}
+                        aria-label="Departure Time"
+                        aria-describedby="Enter departure time of final day"
+                        onChange={handleFieldChange}
+                        required
+                    />
+                    <Form.Control.Feedback type="invalid">Please select a departure time</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group controlId="hasCar">
+                    <ButtonGroup className="d-flex d-flex justify-content-center mt-4">
+                        <Button
+                            variant={hasCar === true ? "success" : "outline-secondary"}
+                            aria-label="Has car"
+                            aria-describedby="Select if you have a car"
+                            onClick={() => handleCarButtonClick(true)}
+                            className="w-50 fw-bold"
+                        >
+                            Car
+                        </Button>
+                        <Button
+                            variant={hasCar === false ? "success" : "outline-secondary"}
+                            aria-label="No car"
+                            aria-describedby="Select if you do not have a car"
+                            onClick={() => handleCarButtonClick(false)}
+                            className="w-50 fw-bold"
+                        >
+                            No Car
+                        </Button>
+                    </ButtonGroup>
+                </Form.Group>
+                
+                <div className="d-flex align-items-center justify-content-center my-4">
                     <Button
-                        key={index + 1}
-                        variant={durationDays === (index + 1) ? "success" : "outline-secondary"}
-                        aria-label={`Duration ${index + 1} Day(s)`}
-                        aria-describedby={`Duration ${index + 1} Day(s)`}
-                        onClick={() => handleDurationButtonClick(index + 1)}
-                        className="d-flex d-flex justify-content-center fw-bold"
+                        type="submit"
+                        aria-label="Generate"
+                        aria-describedby="Generate a vacation plan"
+                        variant="primary"
+                        className="w-50"
+                        disabled={loading}
                     >
-                        {index + 1}
+                        {loading ? (
+                            <>
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                                <span className="visually-hidden">
+                                    GENERATING...
+                                </span>
+                            </>
+                            ) : (
+                                "GENERATE"
+                                )}
                     </Button>
+                </div>
+            </Form>
+
+            {generatedPlan && (
+                <Row xs={1} md={2}>
+                    {generatedPlan.map((dayActivities, dayIndex) => (
+                        <Col key={dayIndex}>   
+                            <Card>
+                                <Card.Body>
+                                    <Card.Header>Day {dayIndex + 1}</Card.Header>
+                                    <ListGroup variant="flush">
+                                        {dayActivities.map((activity) => (
+                                            <ListGroup.Item>
+                                                <strong>{activity.timeOfDay + " | " + activity.activityName}</strong>
+                                                <br />
+                                                {activity.description}
+                                                <br />
+                                                <a
+                                                    href={"https://maps.google.com/?q=" + encodeURIComponent(activity.address).replace(/%20/g, '+')}
+                                                    target="_blank"
+                                                    rel="noreferrer"
+                                                >
+                                                    <small>{activity.address}</small>
+                                                </a>
+                                            </ListGroup.Item>
+                                        ))}
+                                    </ListGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
                     ))}
-                </ButtonGroup>
-            </Form.Group>
-
-            <Form.Group controlId="arrivalTime">
-                <Form.Label className="fw-bold mt-3">Arrival Time</Form.Label>
-                <Form.Control
-                    type="time"
-                    name="arrivalTime"
-                    value={formData.arrivalTime}
-                    aria-label="Arrival Time"
-                    aria-describedby="Enter arrival time of first day"
-                    onChange={handleFieldChange}
-                    required
-                />
-                <Form.Control.Feedback type="invalid">Please select an arrival time</Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group controlId="departureTime">
-                <Form.Label className="fw-bold mt-3">Departure Time</Form.Label>
-                <Form.Control
-                    type="time"
-                    name="departureTime"
-                    value={formData.departureTime}
-                    aria-label="Departure Time"
-                    aria-describedby="Enter departure time of final day"
-                    onChange={handleFieldChange}
-                    required
-                />
-                <Form.Control.Feedback type="invalid">Please select a departure time</Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group controlId="hasCar">
-                <ButtonGroup className="d-flex d-flex justify-content-center mt-4">
-                    <Button
-                        variant={hasCar === true ? "success" : "outline-secondary"}
-                        aria-label="Has car"
-                        aria-describedby="Select if you have a car"
-                        onClick={() => handleCarButtonClick(true)}
-                        className="w-50 fw-bold"
-                    >
-                        Car
-                    </Button>
-                    <Button
-                        variant={hasCar === false ? "success" : "outline-secondary"}
-                        aria-label="No car"
-                        aria-describedby="Select if you do not have a car"
-                        onClick={() => handleCarButtonClick(false)}
-                        className="w-50 fw-bold"
-                    >
-                        No Car
-                    </Button>
-                </ButtonGroup>
-            </Form.Group>
-            
-            <div className="d-flex align-items-center justify-content-center my-4">
-                <Button
-                    type="submit"
-                    aria-label="Generate"
-                    aria-describedby="Generate a vacation plan"
-                    variant="primary"
-                    className="w-50"
-                    disabled={loading}
-                >
-                    {loading ? "GENERATING..." : "GENERATE"}
-                </Button>
-            </div>
-        </Form>
+                </Row>
+            )}
+        </>
     );
 }
