394c22d78bccbc2bf4e8e0749bc2e882fc69a61a
macarious <63441014+macarious@users.noreply.github.com>
2023-06-27 07:58:18 -0700

    Update AuthDebugger.jsx

--- client/src/components/AuthDebugger.jsx
+++ client/src/components/AuthDebugger.jsx
@@ -6,18 +6,18 @@ export default function AuthDebugger() {
   const { user } = useAuth0();
   const { accessToken } = useAuthToken();
 
-  return (
-    <Container style={{ marginTop: '10vh'}} className="d-flex flex-column justify-content-left mb-4">
-    <div className="m-3">
-      <div>
-        <p>Access Token:</p>
-        <pre className="text-wrap">{JSON.stringify(accessToken, null, 2)}</pre>
-      </div>
-      <div>
-        <p>User Info:</p>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-      </div>
-    </div>
-    </ Container>
-  );
+    return (
+        <Container style={{ marginTop: '10vh'}} className="d-flex flex-column justify-content-left mb-4">
+            <div className="m-3">
+                <div>
+                    <p>Access Token:</p>
+                    <pre className="text-wrap">{JSON.stringify(accessToken, null, 2)}</pre>
+                </div>
+                <div>
+                    <p>User Info:</p>
+                    <pre>{JSON.stringify(user, null, 2)}</pre>
+                </div>
+            </div>
+        </ Container>
+    );
 }
